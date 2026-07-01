e6ace65b298de2aca53ee6db86cee532e78627bf
Disverse <sobmi05@gmail.com>
2026-07-01 13:39:56 +0300

    Задание 4

---
+++ 4_WorkWithText/WorkWithText_4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WorkWithText_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string fileName = "receipt.txt";
+            using (var writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine("2026-07-01 14:30:00");
+                writer.WriteLine("Sofa 1300");
+                writer.WriteLine("Table 400,50");
+                writer.WriteLine("Chair 89,99");
+                writer.WriteLine("Bed 2100");
+                writer.WriteLine("Closet 1600");
+            }
+
+            Console.WriteLine($"\tReceipt file {fileName} was created");
+
+            Console.WriteLine($"\n\tOutput with current culture ({CultureInfo.CurrentCulture.Name})");
+            PrintReceipt(fileName, CultureInfo.CurrentCulture);
+
+            var cultureUS = new CultureInfo("en-US");
+            Console.WriteLine($"\n\tOutput with culture {cultureUS.Name}");
+            PrintReceipt(fileName, cultureUS);
+        }
+
+        static void PrintReceipt(string fileName, CultureInfo culture)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                string date = reader.ReadLine();
+                if (DateTime.TryParse(date, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime purchaseDate))
+                {
+                    Console.WriteLine($"Purchase date: {purchaseDate.ToString("D", culture)}");
+                }
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(' ');
+                    string itemName = parts[0];
+                    if (double.TryParse(parts[1], NumberStyles.Currency, CultureInfo.InvariantCulture, out double itemCost))
+                    {
+                        Console.WriteLine($"{itemName}: {itemCost.ToString("C", culture)}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 4_WorkWithText/WorkWithText_4_additional/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WorkWithText_4_additional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tUser registration");
+
+            string login = GetValidLogin();
+            string password = GetValidPassword();
+        }
+
+        static string GetValidLogin()
+        {
+            string login = null;
+            string pattern = @"^[a-zA-z]+$";
+            while (string.IsNullOrEmpty(login))
+            {
+                Console.Write("Enter user login (only latin characters): ");
+                login = Console.ReadLine().Trim();
+                
+                if (Regex.IsMatch(login, pattern))
+                {
+                    break;
+                } 
+                else
+                {
+                    Console.WriteLine("Login may consist only of latin characters. Try again");
+                    login = null;
+                }
+            }
+            return login;
+        }
+
+        static string GetValidPassword()
+        {
+            string password = null;
+            string pattern = @"^[^a-zA-Zа-яА-ЯёЁ\s]{2,}$";
+            while (string.IsNullOrEmpty(password))
+            {
+                Console.Write("Enter user password (only digits and special characters): ");
+                password = Console.ReadLine().Trim();
+                if (Regex.IsMatch(password, pattern)
+                    && Regex.IsMatch(password, @"\D"))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Password may consist only of digits and special characters. Try again");
+                    password = null;
+                }
+            }
+            return password;
+        }
+    }
+}
